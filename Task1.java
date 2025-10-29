892f04df014197c1c041c04fa765eba84a20a3d0
chandramowleechyzizu-web <chandramowleechyzizu@gmail.com>
2025-10-29 23:12:29 +0600

    25-62847-2

---
+++ Task1.java
@@ -0,0 +1,13 @@
+public class Task1{
+     
+     public static void main (String[] args){
+       int x = 70;
+       int y = 72;
+       int z = 74;
+       int sum = x + y + z;
+       System.out.println("Sum is : " + sum + " Average is : " + (sum/3));
+
+    
+    
+}
+}
---
+++ Task3.java
@@ -0,0 +1,11 @@
+public class Task3 {
+
+    public static void main(String[] args){
+        int C = 25 ;
+        float F = 32 + (C*(180/100));
+        System.out.println("In celcius: " + C +" Degree = " + F + " In Farhenheit");
+
+    
+    
+}
+}
